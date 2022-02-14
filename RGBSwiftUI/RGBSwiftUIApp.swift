91be3e94597109a0bf18a74ba8205666d89a53b0
aldvs <andrewgogol777@gmail.com>
2022-02-14 11:19:10 +0300

    Initial Commit

---
+++ RGBSwiftUI/RGBSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGBSwiftUIApp.swift
+//  RGBSwiftUI
+//
+//  Created by admin on 14.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct RGBSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
